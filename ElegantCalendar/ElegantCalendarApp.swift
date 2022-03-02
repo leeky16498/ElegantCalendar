8a54c3ba53eba653c1b739b8da4b7c509822d253
Kyungyun Lee, IOS Dev <leeky16498@gmail.com>
2022-03-02 03:42:24 +0000

    Initial Commit

---
+++ ElegantCalendar/ElegantCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElegantCalendarApp.swift
+//  ElegantCalendar
+//
+//  Created by Kyungyun Lee on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElegantCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
